e2d1549cddffbe58df8a1f4bd9581e4b81e0bb43
Ruslan Pidhainy <ruslanpidhainyi02@gmail.com>
2022-12-03 18:25:57 +0100

    Dodaj pliki projektów.

---
+++ Sneakers_shop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sneakers_shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
